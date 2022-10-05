cd2ffffc7adabc7dfeb19a63749de5a01991cad6
Manu <manuel.munoz.sanchez@gmail.com>
2022-10-05 16:34:01 +0200

    Fix data paths

--- code/bash/rdlm_prerender_test.sh
+++ code/bash/rdlm_prerender_test.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=-1 \
 python /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/prerender/prerender.py \
-  --data-path "/home/manolotis/datasets/waymo/motion v1.0/uncompressed/tf_example/testing/" \
+  --data-path "/media/disk1/datasets/waymo/motion v1.0/uncompressed/tf_example/testing/" \
   --output-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/testing/" \
   --config "/home/manolotis/sandbox/robustness_benchmark/multipathPP/code/configs/prerender.yaml" \
   --n-jobs 48
