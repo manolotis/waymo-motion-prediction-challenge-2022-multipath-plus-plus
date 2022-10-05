9709e59777779690a0988ef4490a43962ee6f99c
Manu <manuel.munoz.sanchez@gmail.com>
2022-10-05 16:25:57 +0200

    Test data prerender script and added training scripts

---
+++ code/bash/rdlm_train.sh
@@ -0,0 +1,5 @@
+python /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/train.py \
+  --config /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/configs/final_RoP_Cov_Single.yaml \
+  --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/training" \
+  --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/validation" \
+  --batch-size 84
---
+++ code/bash/zbook_train.sh
@@ -0,0 +1,6 @@
+python /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/train.py \
+  --config /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/configs/final_RoP_Cov_Single.yaml \
+  --train-data-path "/home/manolotis/sandbox/robustness_benchmark/multipathPP/data/prerendered/training" \
+  --val-data-path "/home/manolotis/sandbox/robustness_benchmark/multipathPP/data/prerendered/validation" \
+  --batch-size 1 \
+  --n-jobs 1
