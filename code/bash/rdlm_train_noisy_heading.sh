18c3cff9848755e39ca3045688674bb5e1adb951
Manu <manuel.munoz.sanchez@gmail.com>
2022-10-28 15:59:25 +0200

    Lower lr and batch size in training configs

--- code/bash/rdlm_train_noisy_heading.sh
+++ code/bash/rdlm_train_noisy_heading.sh
@@ -1,8 +1,8 @@
 python /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/train.py \
-  --config /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/configs/final_RoP_Cov_Single_lr4e-4_noisy_heading.yaml \
+  --config /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/configs/final_RoP_Cov_Single_noisy_heading.yaml \
   --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/training" \
   --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/validation" \
-  --train-data-path-noisy "/home/manolotis/datasets/waymo/motion v1.0/prerender/motionCNN/training_noisy_heading/" \
-  --val-data-path-noisy "/home/manolotis/datasets/waymo/motion v1.0/prerender/motionCNN/validation_noisy_heading/" \
-  --batch-size 84 \
+  --train-data-path-noisy "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/training_noisy_heading/" \
+  --val-data-path-noisy "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/validation_noisy_heading/" \
+  --batch-size 64 \
   --n-shards 16
