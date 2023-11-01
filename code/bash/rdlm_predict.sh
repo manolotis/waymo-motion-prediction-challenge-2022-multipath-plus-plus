dabd902cf3efd2c24dcc2f31d8edf93e08a33002
manolotis <manuel.munoz.sanchez@gmail.com>
2023-11-01 15:32:48 +0100

    Updated RDLM predict script to match files of new project

--- code/bash/rdlm_predict.sh
+++ code/bash/rdlm_predict.sh
@@ -1,8 +1,8 @@
-python /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/predict.py \
-  --config /home/manolotis/sandbox/robustness_benchmark/multipathPP/code/configs/predict.yaml \
+python /home/manolotis/sandbox/scenario_based_evaluation/multipathPP/code/predict.py \
+  --config /home/manolotis/sandbox/scenario_based_evaluation/multipathPP/code/configs/predict.yaml \
   --test-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/testing" \
   --batch-size 64 \
   --n-jobs 32 \
   --n-shards 1 \
-  --out-path "/home/manolotis/sandbox/robustness_benchmark/multipathPP/predictions/"\
+  --out-path "/home/manolotis/sandbox/scenario_based_evaluation/multipathPP/predictions/"\
   --model-name "final_RoP_Cov_Single__18c3cff"
